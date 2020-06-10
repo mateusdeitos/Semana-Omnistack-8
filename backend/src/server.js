31c7ce57911a751f0fd859be155967aad6d8d771
Mateus Deitos <matdeitos@gmail.com>
2020-06-09 21:43:55 -0300

    Omnistack 8.0 -> Aula 1 -> Construindo o back-end

---
+++ backend/src/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const routes = require('./routes');
+const mongoose = require('mongoose');
+
+const server = express();
+mongoose.connect('mongodb+srv://mateus:mateus@cluster0-ygrcy.mongodb.net/omnistack8?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+server.use(cors());
+server.use(express.json());
+server.use(routes);
+
+
+server.listen(3333);
